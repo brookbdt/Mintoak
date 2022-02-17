2a86568a871eb01783434090eeb4a585888fb4cc
urvesh.dungrani <urvesh.dungrani@dataphion.com>
2022-02-18 00:14:05 +0530

    mobile header done

--- components/headers/MiniHeader.js
+++ components/headers/MiniHeader.js
@@ -4,6 +4,149 @@ import Image from "next/image";
 export default function MiniHeader(headerProps) {
   const { showMobileMenu, setShowMobileMenu } = headerProps;
   const [activeMenu, setActiveMenu] = useState("menu");
+  const [activeProductsMenu, setActiveProductsMenu] = useState("");
+
+  const renderPaymentsStack = () => {
+    return (
+      <>
+        {/* Payments Stack start */}
+        <div className="flex pl-8 py-15 items-center justify-between cursor-pointer" onClick={() => setActiveProductsMenu("ps")}>
+          <div className="text-s14l17 text-smokyBlack shrink-0">Payments Stack</div>
+          <i className={"fa-solid fa-angle-down text-atlantis smooth " + (activeProductsMenu === "ps" ? "origin-center rotate-180" : "")} />
+        </div>
+
+        <div className={"smooth " + (activeProductsMenu === "ps" ? "h-175" : "h-0")}>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/omni.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">OMNI</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/mom.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">MOM</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/pay_later.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">Pay Later</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/acron.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">ACORN</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/connect.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">CONNECT</div>
+          </div>
+        </div>
+        {/* Payments Stack end */}
+
+        <div className="w-100% h-1 bg-atlantis opacity-20" />
+
+        {/* Value Added Services start */}
+        <div className="flex pl-8 py-15 items-center justify-between cursor-pointer" onClick={() => setActiveProductsMenu("vas")}>
+          <div className="text-s14l17 text-smokyBlack shrink-0">Value Added Services</div>
+          <i className={"fa-solid fa-angle-down text-atlantis smooth " + (activeProductsMenu === "vas" ? "origin-center rotate-180" : "")} />
+        </div>
+        <div className={"smooth " + (activeProductsMenu === "vas" ? "h-315" : "h-0")}>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/iku.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">IKU</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/oar.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">OAR</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/aim.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">AIM</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/mac.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">MAC</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/uar.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">UAR</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/bat.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">BAT</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/far.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">FAR</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/sam.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">SAM</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/pam.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">PAM</div>
+          </div>
+        </div>
+        {/* Value Added Services end */}
+
+        <div className="w-100% h-1 bg-atlantis opacity-20" />
+
+        {/* Lending Stack start */}
+        <div className="flex pl-8 py-15 items-center justify-between cursor-pointer" onClick={() => setActiveProductsMenu("ls")}>
+          <div className="text-s14l17 text-smokyBlack shrink-0">Lending Stack</div>
+          <i className={"fa-solid fa-angle-down text-atlantis smooth " + (activeProductsMenu === "ls" ? "origin-center rotate-180" : "")} />
+        </div>
+
+        <div className={"smooth " + (activeProductsMenu === "ls" ? "h-70" : "h-0")}>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/xsm.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">XSM</div>
+          </div>
+          <div className="flex pl-8 pb-15 items-center cursor-pointer">
+            <Image src="/icons/xsc.svg" alt="blog" height={20} width={20} />
+            <div className="text-s12l14 text-nero shrink-0 ml-8">XSC</div>
+          </div>
+        </div>
+        {/* Lending Stack end */}
+
+        <div className="w-100% h-1 bg-atlantis opacity-20" />
+      </>
+    );
+  };
+
+  const renderResources = () => {
+    return (
+      <>
+        <div className="flex pl-8 py-9 pt-15 items-center cursor-pointer">
+          <Image src="/icons/blog.svg" alt="blog" height={20} width={20} />
+          <div className="text-s12l14 text-nero shrink-0 ml-8">Blog</div>
+        </div>
+
+        <div className="flex pl-8 py-9 items-center cursor-pointer">
+          <Image src="/icons/case_studies.svg" alt="case_studies" height={20} width={20} />
+          <div className="text-s12l14 text-nero shrink-0 ml-8">Case Studies</div>
+        </div>
+
+        <div className="flex pl-8 py-9 items-center cursor-pointer">
+          <Image src="/icons/in_the_media.svg" alt="in_the_media" height={20} width={20} />
+          <div className="text-s12l14 text-nero shrink-0 ml-8">In the media</div>
+        </div>
+      </>
+    );
+  };
+
+  const renderSupport = () => {
+    return (
+      <>
+        <div className="flex pl-8 py-9 pt-15 items-center cursor-pointer">
+          <Image src="/icons/faqs.svg" alt="faqs" height={20} width={20} />
+          <div className="text-s12l14 text-nero shrink-0 ml-8">FAQs</div>
+        </div>
+
+        <div className="flex pl-8 py-9 items-center cursor-pointer">
+          <Image src="/icons/contact_us.svg" alt="contact_us" height={20} width={20} />
+          <div className="text-s12l14 text-nero shrink-0 ml-8">Contact Us</div>
+        </div>
+      </>
+    );
+  };
 
   return (
     <>
@@ -27,7 +170,13 @@ export default function MiniHeader(headerProps) {
 
         <div className="mx-16 h-100% w-205">
           {/* Menu title */}
-          <div className="flex items-center" onClick={() => setActiveMenu("menu")}>
+          <div
+            className="flex items-center"
+            onClick={() => {
+              setActiveMenu("menu");
+              setActiveProductsMenu("");
+            }}
+          >
             {activeMenu !== "menu" && <i className="fa-solid fa-angle-left text-atlantis" />}
             <div className="ml-8 text-s12l14 text-matterhorn capitalize">{activeMenu}</div>
           </div>
@@ -75,59 +224,13 @@ export default function MiniHeader(headerProps) {
 
         {/* Active menu sidebar */}
         <div className={"absolute right-0 top-70 mx-16 h-100% smooth bg-hawkesBlue " + (activeMenu !== "menu" ? "w-205" : "w-0")}>
-          {activeMenu === "products" ? (
-            <>
-              <div className="flex pl-8 py-15 items-center justify-between cursor-pointer">
-                <div className="text-s14l17 text-smokyBlack shrink-0">Payments Stack</div>
-                <i className="fa-solid fa-angle-down text-atlantis" />
-              </div>
-
-              <div className="w-100% h-1 bg-atlantis opacity-20" />
-
-              <div className="flex pl-8 py-15 items-center justify-between cursor-pointer">
-                <div className="text-s14l17 text-smokyBlack shrink-0">Value Added Services</div>
-                <i className="fa-solid fa-angle-down text-atlantis" />
-              </div>
-
-              <div className="w-100% h-1 bg-atlantis opacity-20" />
-
-              <div className="flex pl-8 py-15 items-center justify-between cursor-pointer">
-                <div className="text-s14l17 text-smokyBlack shrink-0">Lending Stack</div>
-                <i className="fa-solid fa-angle-down text-atlantis" />
-              </div>
-
-              <div className="w-100% h-1 bg-atlantis opacity-20" />
-            </>
-          ) : activeMenu === "resources" ? (
-            <>
-              <div className="flex pl-8 py-9 pt-15 items-center cursor-pointer">
-                <Image src="/icons/blog.svg" alt="blog" height={20} width={20} />
-                <div className="text-s12l14 text-nero shrink-0 ml-8">Blog</div>
-              </div>
-
-              <div className="flex pl-8 py-9 items-center cursor-pointer">
-                <Image src="/icons/case_studies.svg" alt="case_studies" height={20} width={20} />
-                <div className="text-s12l14 text-nero shrink-0 ml-8">Case Studies</div>
-              </div>
-
-              <div className="flex pl-8 py-9 items-center cursor-pointer">
-                <Image src="/icons/in_the_media.svg" alt="in_the_media" height={20} width={20} />
-                <div className="text-s12l14 text-nero shrink-0 ml-8">In the media</div>
-              </div>
-            </>
-          ) : activeMenu === "support" ? (
-            <>
-              <div className="flex pl-8 py-9 pt-15 items-center cursor-pointer">
-                <Image src="/icons/faqs.svg" alt="faqs" height={20} width={20} />
-                <div className="text-s12l14 text-nero shrink-0 ml-8">FAQs</div>
-              </div>
-
-              <div className="flex pl-8 py-9 items-center cursor-pointer">
-                <Image src="/icons/contact_us.svg" alt="contact_us" height={20} width={20} />
-                <div className="text-s12l14 text-nero shrink-0 ml-8">Contact Us</div>
-              </div>
-            </>
-          ) : null}
+          {activeMenu === "products"
+            ? renderPaymentsStack()
+            : activeMenu === "resources"
+            ? renderResources()
+            : activeMenu === "support"
+            ? renderSupport()
+            : null}
         </div>
       </div>
     </>
