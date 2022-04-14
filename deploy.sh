3eefed79de97d092d845b4f78c6f356e2c63af13
urvesh.dungrani <urvesh.dungrani@dataphion.com>
2022-04-14 13:49:57 +0530

    script

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,21 @@ git pull
 if [ $? -eq 0 ]; then
     echo 'Git pulled successfully'
     echo 'Rebuilding Project...'
-    npm install
-    npm run build
+    # npm install
+    # npm run build
 
     echo 'Restarting PM2...'
-    pm2 restart 0
-
-    echo 'Mintoak Deployed.'
+    pm2 restart mintOak
+    if [ $? -eq 0 ]; then
+        echo 'Mintoak Deployed.'
+    else
+        echo 'PM2 not found, installing PM2...'
+        npm i -g pm2
+        echo 'PM2 starting mintOak...'
+        pm2 delete mintOak
+        pm2 start --name=mintOak "npm start"
+        echo 'Mintoak Deployed.'
+    fi
 else
     echo FAIL
 fi
