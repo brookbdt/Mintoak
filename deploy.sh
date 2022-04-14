f2df89f7612766be50f8a1db4180714d4f777a98
urvesh.dungrani <urvesh.dungrani@dataphion.com>
2022-04-14 13:33:42 +0530

    script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-echo 'Deploying Mintoak'
+echo 'Deploying Mintoak...'
 git pull
+if [ $? -eq 0 ]; then
+    echo 'Git pulled successfully'
+    echo 'Rebuilding Project...'
+else
+    echo FAIL
+fi
 
 npm install
 npm run build
 
-echo 'Restarting PM2'
+echo 'Restarting PM2...'
 pm2 restart 0
 
-echo 'Mintoak Deployed'
+echo 'Mintoak Deployed.'
