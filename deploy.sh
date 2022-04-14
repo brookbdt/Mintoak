aa56966c1087f3b25def3d0db53ebaa4a91d5571
urvesh.dungrani <urvesh.dungrani@dataphion.com>
2022-04-14 13:50:11 +0530

    script

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ git pull
 if [ $? -eq 0 ]; then
     echo 'Git pulled successfully'
     echo 'Rebuilding Project...'
-    # npm install
-    # npm run build
+    npm install
+    npm run build
 
     echo 'Restarting PM2...'
     pm2 restart mintOak
